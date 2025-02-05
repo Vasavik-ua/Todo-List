9a87f8b4db4cf84289458f75e25b36906e9e22b7
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-02-05 11:11:10 +0100

    add a class list view

--- task/views.py
+++ task/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from task.models import Task
+
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = "task_list"
+    ordering = ['progress', '-datetime']
