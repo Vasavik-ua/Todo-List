8fc9457914c4a644280071367f82af2e9d2ec29a
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-02-05 11:12:11 +0100

    add two models for task ang tags objects

--- task/models.py
+++ task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField(max_length=255, blank=False)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline_datetime = models.DateTimeField(blank=True)
+    progress = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", name="tags", blank=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
